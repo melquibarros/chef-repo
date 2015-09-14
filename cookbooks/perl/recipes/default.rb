208e2991e320364f60159fadd461f51a7b471376
ildefonso <ildefonso.barros@ipsense.com.br>
2015-09-14 16:30:37 -0300

    receita perl renomeada para perl-install, devido a conflito com a receita perl do supermarket criado cookbook "diretorios", para automatizar a criação de diretórios e serviços

--- cookbooks/perl/recipes/default.rb
+++ cookbooks/perl/recipes/default.rb
@@ -2,23 +2,42 @@
 # Cookbook Name:: perl
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2009-2013, Chef Software, Inc.
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-package 'epel-release' do #requisito para instalação do mod_perl
-  action :install
-end
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-package 'perl-Bit-Vector' do
-  action :install
-end
-package 'perl-CPAN' do
-  action :install
-end
-package 'mod_perl' do
-  action :install
-end
-package 'perl-CGI' do
-  action :install
+case node['platform']
+when 'windows'
+  include_recipe 'perl::_windows'
+
+else
+  node['perl']['packages'].each do |perl_pkg|
+    package perl_pkg
+  end
+
+  cpanm = node['perl']['cpanm'].to_hash
+  root_group = node['platform'] == 'mac_os_x' ? 'admin' : 'root'
+
+  directory File.dirname(cpanm['path']) do
+    recursive true
+  end
+
+  remote_file cpanm['path'] do
+    source cpanm['url']
+    checksum cpanm['checksum']
+    owner 'root'
+    group root_group
+    mode 0755
+  end
 end
--- roles/jboss.rb
+++ roles/jboss.rb
@@ -1,3 +1,3 @@
 name "jboss"
 description "Webservers jboss(rjconsultores)"
-run_list "role[base]", "recipe[cpan]", "recipe[cpan::Modules]", "recipe[perl]", "recipe[gcc]", "recipe[glibc]", "recipe[binutils]", "recipe[cpp]", "recipe[ssl]", "recipe[libgd]", "recipe[tools]","recipe[java]"
+run_list "role[base]","recipe[perl-install]","recipe[gcc]","recipe[glibc]","recipe[binutils]","recipe[cpp]","recipe[ssl]","recipe[libgd]","recipe[tools]","recipe[java]", "recipe[diretorios]"
